66150b9f473dccbbc57a6e64b488e95f63bf2965
Arte Ebrahimi <arteebrahimi@gmail.com>
2022-09-26 21:48:48 -0700

    app bar

--- client/src/App.js
+++ client/src/App.js
@@ -1,35 +1,43 @@
 import React from 'react'
-import { Container, AppBar, Typography, Grow, Grid } from '@mui/material'
+import { Container, AppBar, Typography, Grow, Grid, Button, Toolbar, Box } from '@mui/material'
+import { ThemeProvider, createTheme } from '@mui/material/styles';
 
 import Form from './components/Form/Form'
 import Posts from './components/Posts/Posts'
 import hookahPic from './images/hookahtable.jpg'
-import appStyles from './styles'
+
+const theme = createTheme({ palette: { mode: 'dark' } })
 
 const App = () => {
-  const classes = appStyles()
+
 
   return (
-    <Container maxWidth='lg'>
-      <AppBar className={classes.root} position="static" color="inherit">
-        <Typography className={classes.heading} variant="h2" align="center">Hookah Reviews</Typography>
-        <img className={classes.image} src={hookahPic} alt="Hookah Review" height="60"></img>
-      </AppBar>
-      <Grow in>
-        <Container>
-          <Grid container justiy="space-between" alignItems="stretch" spacing={3}>
-            <Grid item xs={12} sm={7}>
-              <Posts />
-            </Grid>
-            <Grid item xs={12} sm={4}>
-              <Form />
-            </Grid>
+    <ThemeProvider theme={theme}>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static" >
+          <Toolbar>
+            <Typography variant="h6" sx={{ flexGrow: 1 }}>Hookah Reviews</Typography>
+            <img src={hookahPic} alt="Hookah Review" height="60"></img>
+          </Toolbar>
+        </AppBar>
+      </Box>
+      <Container maxWidth="lg">
+        <Grow in>
+          <Container>
+            <Grid container justiy="space-between" alignItems="stretch" spacing={3}>
+              <Grid item xs={12} sm={7}>
+                <Posts />
+              </Grid>
+              <Grid item xs={12} sm={4}>
+                <Form />
+              </Grid>
 
-          </Grid>
-        </Container>
+            </Grid>
+          </Container>
 
-      </Grow>
-    </Container>
+        </Grow>
+      </Container>
+    </ThemeProvider>
   )
 }
 
