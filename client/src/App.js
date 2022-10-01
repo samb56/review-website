f631339627feaeea0b2fbc6be9fd1d2824e689bd
Sam <2726-sambergeland-WKCcKe@users.noreply.uci.bootcampcontent.com>
2022-09-30 18:51:10 -0700

    added beginning structure of landing page

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,35 @@
 import React from 'react'
+import { Container, AppBar, Typography, Grow, Grid } from '@mui/material'
+
+import Posts from './components/Posts/Posts'
+import Form from './components/Form/Form'
+import memories from './images/hookah clouds.jpg'
 
 const App = () => {
 
   return (
-    <div>
-      <h1>APP</h1>
-    </div>
+    <Container maxWidth='lg'>
+      <AppBar position='static' color='inherit'>
+        <Typography variant='h2' align="center">Memories</Typography>
+        <img src={memories} alt="memories" height="60" />
+      </AppBar>
+
+      <Grow in>
+        <Container>
+          <Grid container justify="space-between" alignItems="stretch" spacing={2}>
+            <Grid item xs={12} sm={7}>
+              <Posts />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Form />
+            </Grid>
+
+          </Grid>
+
+        </Container>
+      </Grow>
+
+    </Container>
   )
 }
 
