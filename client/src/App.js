265ce405667a7b67ec9beb2a7859ba95e591bd96
Arte Ebrahimi <arteebrahimi@gmail.com>
2022-10-01 14:07:53 -0700

    v5 styling examples

--- client/src/App.js
+++ client/src/App.js
@@ -1,38 +1,61 @@
-import React from 'react'
-import { Container, AppBar, Typography, Grow, Grid } from '@mui/material'
+import React from 'react';
+import { Container, AppBar, Typography, Grow, Grid, Box } from '@mui/material';
+import CssBaseline from '@mui/material/CssBaseline';
+import { ThemeProvider } from '@mui/material/styles';
 
-import Posts from './components/Posts/Posts'
-import Form from './components/Form/Form'
-import memories from './images/hookah clouds.jpg'
-import useStyles from './styles'
+import Posts from './components/Posts/Posts';
+
+import Form from './components/Form/Form';
+import memories from './images/hookah clouds.jpg';
+import theme from './theme';
+import useStyles, { StyledAppBar, sxStyles } from './styles';
 
 const App = () => {
-  const { classes } = useStyles()
-
+  const { classes } = useStyles();
   return (
-    <Container maxWidth='lg'>
-      <AppBar className={classes.appBar} position='static' color='inherit'>
-        <Typography className={classes.heading} variant='h2' align="center">Memories</Typography>
-        <img className={classes.image} src={memories} alt="memories" height="60" />
-      </AppBar>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Container maxWidth="lg">
+        {
+          // Old useStyles/makeStyles API
+        }
+        <AppBar className={classes.appBar} position="static" color="inherit">
+          <Typography className={classes.heading} variant="h2" align="center">
+            Memories
+          </Typography>
+          <img className={classes.image} src={memories} alt="memories" height="60" />
+        </AppBar>
 
-      <Grow in>
-        <Container>
-          <Grid container justify="space-between" alignItems="stretch" spacing={2}>
-            <Grid item xs={12} sm={7}>
-              <Posts />
+        {
+          // Styled API
+        }
+        <StyledAppBar />
+
+        {
+          // sx props
+        }
+        <AppBar position="static" color="inherit" sx={sxStyles.appBar}>
+          <Typography variant="h2" align="center" sx={sxStyles.header}>
+            Memories
+          </Typography>
+          <Box component="img" src={memories} alt="memories" sx={sxStyles.image} />
+        </AppBar>
+
+        <Grow in>
+          <Container>
+            <Grid container justify="space-between" alignItems="stretch" spacing={2}>
+              <Grid item xs={12} sm={7}>
+                <Posts />
+              </Grid>
+              <Grid item xs={12} sm={4}>
+                <Form />
+              </Grid>
             </Grid>
-            <Grid item xs={12} sm={4}>
-              <Form />
-            </Grid>
+          </Container>
+        </Grow>
+      </Container>
+    </ThemeProvider>
+  );
+};
 
-          </Grid>
-
-        </Container>
-      </Grow>
-
-    </Container>
-  )
-}
-
-export default App
+export default App;
--- client/src/styles.js
+++ client/src/styles.js
@@ -1,9 +1,21 @@
-// import { makeStyles } from "@mui/styles";
-import { makeStyles } from 'tss-react/mui'
+import { makeStyles } from 'tss-react/mui';
+import { AppBar, Typography } from '@mui/material';
+import { styled } from '@mui/material/styles';
 
-export default makeStyles() ((theme) => {
-  return {
-  appBar: {
+import memories from './images/hookah clouds.jpg';
+
+const MainAppBar = (props) => (
+  <AppBar position="static" color="inherit" {...props}>
+    <Typography className="heading" variant="h2" align="center">
+      Memories
+    </Typography>
+    <img className="image" src={memories} alt="memories" height="60" />
+  </AppBar>
+);
+
+// Method 1 using styled API
+export const StyledAppBar = styled(MainAppBar)(() => ({
+  '&': {
     borderRadius: 15,
     margin: '30px 0',
     display: 'flex',
@@ -11,11 +23,48 @@ export default makeStyles() ((theme) => {
     justifyContent: 'center',
     alignItems: 'center',
   },
-  heading: {
+  '& .heading': {
+    color: 'rgba(0,183,255, 1)',
+  },
+  '& .image': {
+    marginLeft: '15px',
+  },
+}));
+
+// Method 2 using sx style props
+export const sxStyles = {
+  appBar: {
+    borderRadius: '15px',
+    margin: '30px 0',
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  header: {
     color: 'rgba(0,183,255, 1)',
   },
   image: {
     marginLeft: '15px',
+    height: '60px',
   },
-}
-})
+};
+
+export default makeStyles()((theme) => {
+  return {
+    appBar: {
+      borderRadius: 15,
+      margin: '30px 0',
+      display: 'flex',
+      flexDirection: 'row',
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+    heading: {
+      color: 'rgba(0,183,255, 1)',
+    },
+    image: {
+      marginLeft: '15px',
+    },
+  };
+});
