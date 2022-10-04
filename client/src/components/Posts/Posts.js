305ec4651bd3a487a5534e04fbe4576ee8c50fc7
Sam <2726-sambergeland-WKCcKe@users.noreply.uci.bootcampcontent.com>
2022-10-04 16:48:25 -0700

    added viewable posts from the submitted information

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Grid, CircularProgress } from '@mui/material'
 import { useSelector } from 'react-redux'
 import Post from './Post/Post'
 import useStyles from './styles'
@@ -10,13 +11,15 @@ const Posts = () => {
   console.log(posts)
   
   return (
-    <>
-    <h1>POSTS</h1>
-    <Post />
-    <Post />
-    <Post />
-    <Post />
-    </>
+    !posts.length ? <CircularProgress /> : (
+      <Grid className={classes.container} container alignItems="stretch" spacing={3}>
+        {posts.map((post) => (
+          <Grid item key={post._id} xs={12} sm={6}>
+              <Post post={post} />
+             </Grid>
+        )) }
+      </Grid>
+    )
   )
 }
 
