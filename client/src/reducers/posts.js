30b53a5e547d7563bf0d792531e50ef7c0359dfc
Sam <2726-sambergeland-WKCcKe@users.noreply.uci.bootcampcontent.com>
2022-10-03 19:56:04 -0700

    added form to submit info

--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -1,12 +1,12 @@
 export default (posts = [], action) => {
- switch (action.type) {
-  case 'FETCH_ALL':
-  
-   return action.payload
-  case 'CREATE':
-    
-   return posts
-  default:
-    return posts
- }
+  switch (action.type) {
+    case 'FETCH_ALL':
+
+      return action.payload
+    case 'CREATE':
+
+      return [...action, action.pyload]
+    default:
+      return posts
+  }
 }
